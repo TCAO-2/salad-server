a3dadc741d78b61147e36cce865066856a50a4e3
raphael <raphael@pop-os.localdomain>
2024-11-10 13:24:50 +0100

    Improve logging and SMART attributes interpretation.

--- scripts/check-disk-health.sh
+++ scripts/check-disk-health.sh
@@ -11,7 +11,7 @@ set -e
 # Parameters
 ################################################################################
 
-DISK_ID=$1 # Device ID as in /dev/disk/by-id
+DISK_ID=$1 # Disk ID as in /dev/disk/by-id
 
 
 
@@ -21,7 +21,7 @@ DISK_ID=$1 # Device ID as in /dev/disk/by-id
 # Constants
 ################################################################################
 
-# These attributes ID are critical i.e. non null values are considered as failures.
+# These ID are critical i.e. non null raw values are considered as failures.
 # https://en.wikipedia.org/wiki/Self-Monitoring,_Analysis_and_Reporting_Technology
 CRITICALS=(
     5       # Reallocated sectors count.
@@ -46,14 +46,14 @@ function logger {
 
 
 
-# Check if the disk is detected as /dev/sdX.
+# Check if the disk is detected as /dev/sdX
 {
     readlink "/dev/disk/by-id/${DISK_ID}" > /dev/null
 } || {
     logger "${DISK_ID} missing" "ERROR"; exit 10
 }
 DEVICE=$(realpath "/dev/disk/by-id/${DISK_ID}")
-logger "${DISK_ID} detected as ${DEVICE}" "TRACE"
+logger "${DISK_ID} detected as ${DEVICE}" "VERB"
 
 
 
@@ -70,9 +70,9 @@ healthy=true
 
 
 # Check SMART status.
-SMART_STATUS=$(smartctl -aj /dev/sda | jq .smart_status.passed)
+SMART_STATUS=$(smartctl -aj $DEVICE | jq .smart_status.passed)
 if [ $SMART_STATUS = "true" ]; then
-    logger "${DISK_ID} SMART status OK" "TRACE"
+    logger "${DISK_ID} SMART status OK" "VERB"
 else
     logger "${DISK_ID} SMART status KO" "ERROR"
     healthy=false
@@ -83,7 +83,7 @@ fi
 # Check SMART selftests.
 SMART_SELF_ERR=$(smartctl -jl selftest $DEVICE | jq .ata_smart_self_test_log.standard.error_count_total)
 if [ $SMART_SELF_ERR = "null" ] || [ $SMART_SELF_ERR -eq 0 ]; then
-    logger "${DISK_ID} SMART selftest OK" "TRACE"
+    logger "${DISK_ID} SMART selftest OK" "VERB"
 else
     logger "${DISK_ID} SMART selftest error count: ${SMART_SELF_ERR}" "ERROR"
     healthy=false
@@ -92,22 +92,35 @@ fi
 
 
 # Check SMART attributes.
-SMART_ATTRIBUTES=$(sudo smartctl -aj $DEVICE | jq .ata_smart_attributes.table)
-for attribute in $(echo "$SMART_ATTRIBUTES" | jq -c '.[] | {id, name, worst, thresh, raw_value: .raw.value}'); do
-    id=$(echo "$attribute" | jq '.id')
-    name=$(echo "$attribute" | jq -r '.name')
-    worst=$(echo "$attribute" | jq '.worst')
-    thresh=$(echo "$attribute" | jq '.thresh')
-    raw_value=$(echo "$attribute" | jq '.raw_value')
-    if [ $worst -le $thresh ]; then
-        logger "${DISK_ID} SMART bad attribute: ${id} ${name} ${worst} <= ${thresh}" "ERROR"
-        healthy=false
+healthy_attributes=true
+SMART_ATTRIBUTES=$(smartctl -aj $DEVICE | jq .ata_smart_attributes.table)
+SMART_JSON=$(echo "$SMART_ATTRIBUTES" | jq -c '.[] | {id, name, value, worst, thresh, raw_value: .raw.value}')
+for attribute in $SMART_JSON; do
+    logger "${DISK_ID} SMART attribute ${attribute}" "TRACE"
+    id=$(echo $attribute | jq '.id')
+    name=$(echo $attribute | jq -r '.name')
+    value=$(echo $attribute | jq '.value')
+    worst=$(echo $attribute | jq '.worst')
+    thresh=$(echo $attribute | jq '.thresh')
+    raw_value=$(echo $attribute | jq '.raw_value')
+    if [ $value -le $thresh ]; then
+        logger "${DISK_ID} SMART bad current attribute: ${id} ${name} ${worst} <= ${thresh}" "ERROR"
+        healthy_attributes=false
+    elif [ $worst -le $thresh ]; then
+        # If the current value is above the threshold
+        # but have been below previously, report a warning only.
+        logger "${DISK_ID} SMART bad worst attribute: ${id} ${name} ${worst} <= ${thresh}" "WARN"
     fi
     if [ $raw_value -ne 0 ] && [[ $(echo "${CRITICALS[@]}" | fgrep -w $id) ]]; then
-        logger "${DISK_ID} SMART bad raw attribute: ${id} ${name} ${raw_value}" "ERROR"
-        healthy=false
+        logger "${DISK_ID} SMART bad raw.value attribute: ${id} ${name} ${raw_value}" "ERROR"
+        healthy_attributes=false
     fi
 done
+if [ $healthy_attributes == true ];then
+    logger "${DISK_ID} SMART attributes OK" "VERB"
+else
+    healthy=false
+fi
 
 
 
--- scripts/logger.sh
+++ scripts/logger.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Handle logging for multiple transports.
+# You can export the min log level for each transport.
+# You can also make these permanent using /etc/environment
+#
+#   | transport | min log level (default)  |
+#   |-----------|--------------------------|
+#   | console   | LOG_LEVEL_CONSOLE (INFO) |
+#   | file      | LOG_LEVEL_FILE    (INFO) |
+#
+# Available log levels in severity order are TRACE VERB INFO WARN ERROR
+#
+# "file" transport logs into /var/log/salad files which are managed by logrotate.
+# See "Configure the logger" section of the server installation script.
+
 # Stop the script on error.
 set -e
 
@@ -11,9 +25,9 @@ set -e
 # Parameters
 ################################################################################
 
-NAME=$1     # Name of the log file.
-MESSAGE=$2  # Log entry.
-LOGLEVEL=$3 # TRACE INFO WARN ERROR
+LOG_NAME=$1     # Name of the log entry (will be in the log file name).
+LOG_MESSAGE=$2  # Log entry.
+LOG_LEVEL=$3    # Log entry level.
 
 
 
@@ -24,7 +38,58 @@ LOGLEVEL=$3 # TRACE INFO WARN ERROR
 ################################################################################
 
 LOG_DIR="/var/log/salad"
-LOG_FILE="${LOG_DIR}/${NAME}.log"
+LOG_FILE="${LOG_DIR}/${LOG_NAME}.log"
+
+# Message colors.
+declare -A LOG_LEVELS=(
+    [TRACE]=0
+    [VERB]=1
+    [INFO]=2
+    [WARN]=3
+    [ERROR]=4
+)
+declare -A LOG_COLORS=(
+    [TRACE]="\033[0;34m"    # BLUE
+    [VERB]="\033[0;36m"     # CYAN
+    [INFO]="\033[0;32m"     # GREEN
+    [WARN]="\033[1;33m"     # YELLOW
+    [ERROR]="\033[0;31m"    # RED
+)
+NC="\033[0m"                # NO COLOR
+
+# Default minimum log levels depending on the transport.
+if [ -z $LOG_LEVEL_CONSOLE ]; then
+    LOG_LEVEL_CONSOLE="INFO"
+fi
+if [ -z $LOG_LEVEL_FILE ]; then
+    LOG_LEVEL_FILE="INFO"
+fi
+
+
+
+
+
+################################################################################
+# Helper functions
+################################################################################
+
+function echo_console {
+    local message="${1}"
+
+    # Check if the log level is within the console display threshold
+    if [[ ${LOG_LEVELS[$LOG_LEVEL]} -ge ${LOG_LEVELS[$LOG_LEVEL_CONSOLE]} ]]; then
+        echo -e "${LOG_COLORS[$LOG_LEVEL]}${message}${NC}"
+    fi
+}
+
+function echo_file {
+    local message="${1}"
+
+    # Check if the log level is within the console display threshold
+    if [[ ${LOG_LEVELS[$LOG_LEVEL]} -ge ${LOG_LEVELS[$LOG_LEVEL_FILE]} ]]; then
+        echo -e "${LOG_COLORS[$LOG_LEVEL]}${message}${NC}" >> $LOG_FILE
+    fi
+}
 
 
 
@@ -34,11 +99,11 @@ LOG_FILE="${LOG_DIR}/${NAME}.log"
 # Main
 ################################################################################
 
-mkdir -p $LOG_DIR
+mkdir -p "${LOG_DIR}"
 timestamp=$(date "+%Y-%m-%d %H:%M:%S")
 
 # Console transport.
-echo -e "[${timestamp} ${LOGLEVEL}]\t${MESSAGE}"
+echo_console "[${timestamp} ${LOG_LEVEL}]\t${LOG_MESSAGE}"
 
 # File transport.
-echo -e "[${timestamp} ${LOGLEVEL}]\t${MESSAGE}" >> $LOG_FILE
+echo_file "[${timestamp} ${LOG_LEVEL}]\t${LOG_MESSAGE}"
